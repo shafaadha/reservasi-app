a669a73d9df3fa8074c055b4cab063874345d8d3
shafaadha <adhashafa@gmail.com>
2025-09-20 18:02:12 +0700

    fix home page

--- resources/src/views/Available.vue
+++ resources/src/views/Available.vue
@@ -1,19 +1,33 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { useRoomStore } from "../stores/roomStore";
+import { useRoute } from "vue-router";
 import api from "../services/api";
 import axios from "axios";
 
-const roomStore = useRoomStore();
+const route = useRoute();
+
 const photos = ref([]);
+const rooms = ref([]);
 const loading = ref(true);
 
 onMounted(async () => {
   try {
+    const { checkin, checkout, guest, room } = route.query;
+
+    // fetch kamar tersedia
+    const response = await api.post("/room/check-availability", {
+      checkin,
+      checkout,
+      guest,
+      room,
+    });
+    rooms.value = response.data.availableRooms || []; // asumsi response ada property availableRooms
+
+    // fetch access key unsplash
     const configRes = await api.get("/config");
     let ACCESS_KEY = configRes.data.unsplash_access_key;
 
-    // fetch photo dari unsplash
+    // fetch photo
     const res = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: "hotel", per_page: 12 },
       headers: { Authorization: `Client-ID ${ACCESS_KEY}` },
@@ -31,19 +45,11 @@ onMounted(async () => {
   <div>
     <h1 class="text-xl font-bold mb-4">Daftar Kamar Tersedia</h1>
 
-    <div v-if="roomStore.availableRooms?.availableRooms?.length">
-      <div
-        v-for="(room, index) in roomStore.availableRooms.availableRooms"
-        :key="room.id"
-        class="flex flex-row justify-between border border-gray-200 rounded p-4 mb-3 shadow h-full w-150"
-      >
-
-        <img
-          v-if="photos[index]"
-          :src="photos[index].urls.small"
-          :alt="photos[index].alt_description"
-          class="w-40 h-32 object-cover rounded-md mx-2"
-        />
+    <div v-if="rooms.length">
+      <div v-for="(room, index) in rooms" :key="room.id"
+        class="flex flex-row justify-between border border-gray-200 rounded p-4 mb-3 shadow h-full w-150">
+        <img v-if="photos[index]" :src="photos[index].urls.small" :alt="photos[index].alt_description"
+          class="w-40 h-32 object-cover rounded-md mx-2" />
 
         <div>
           <h2 class="font-semibold text-lg">{{ room.name }}</h2>
@@ -51,7 +57,6 @@ onMounted(async () => {
           <p>Kapasitas: {{ room.capacity }} orang</p>
           <p>Jumlah tersedia: {{ room.quantity }}</p>
         </div>
-
 
         <div class="text-green-600 font-bold text-right mt-4">
           Rp {{ Number(room.price).toLocaleString("id-ID") }}
--- resources/src/views/Home.vue
+++ resources/src/views/Home.vue
@@ -7,45 +7,52 @@
 
             <!-- Card -->
             <div class="absolute left-1/2 bottom-0 transform -translate-x-1/2 translate-y-1/2 
-                  bg-white shadow-lg rounded-2xl p-8 z-10">
-                <div class="flex space-x-8">
-                    <div class="flex flex-col items-start space-y-2">
+            bg-white shadow-lg rounded-2xl p-6 md:p-8 z-10 w-[90%] md:w-auto">
+                <div class="grid grid-cols-1 sm:grid-cols-7 gap-4 sm:gap-6 items-end">
+
+                    <!-- Check In -->
+                    <div class="col-span-1 sm:col-span-2 flex flex-col space-y-1">
                         <label for="checkin" class="text-gray-800 font-medium">Check In</label>
                         <input v-model="checkin" type="date" id="checkin"
-                            class="h-12 px-4 rounded-md border border-gray-300">
+                            class="w-full h-12 px-4 rounded-md border border-gray-300">
                     </div>
-                    <div class="flex flex-col items-start space-y-2">
+
+                    <!-- Check Out -->
+                    <div class="col-span-1 sm:col-span-2 flex flex-col space-y-1">
                         <label for="checkout" class="text-gray-800 font-medium">Check Out</label>
                         <input v-model="checkout" type="date" id="checkout"
-                            class="h-12 px-4 rounded-md border border-gray-300">
+                            class="w-full h-12 px-4 rounded-md border border-gray-300">
                     </div>
-                    <div class="flex flex-col items-start space-y-2">
+
+                    <!-- Guest -->
+                    <div class="col-span-1 sm:col-span-1 flex flex-col space-y-1">
                         <label for="guest" class="text-gray-800 font-medium">Guest</label>
                         <input v-model="guest" type="number" id="guest"
-                            class="h-12 px-4 rounded-md border border-gray-300">
+                            class="w-full h-12 px-4 rounded-md border border-gray-300">
                     </div>
-                    <div class="flex flex-col items-start space-y-2">
+
+                    <!-- Room -->
+                    <div class="col-span-1 sm:col-span-1 flex flex-col space-y-1">
                         <label for="room" class="text-gray-800 font-medium">Room</label>
                         <input v-model="room" type="number" id="room"
-                            class="w-28 h-12 px-4 rounded-md border border-gray-300">
+                            class="w-full h-12 px-4 rounded-md border border-gray-300">
                     </div>
-                    <!-- Tombol Search -->
-                    <div class="flex justify-end mt-6">
-                        <button @click="handleSearch"
-                            class="bg-cyan-500 text-white px-6 py-2 rounded-lg hover:bg-cyan-600">
-                            Find
-                        </button>
-                    </div>
-                </div>
 
+                    <!-- Tombol Find -->
+                    <button class="col-span-1 sm:col-span-1 bg-cyan-100 rounded-md p-1 flex items-center justify-center"
+                        v-on:click="handleSearch">
+                        <MagnifyingGlassCircleIcon class="w-6 h-6 text-cyan-700" />
+                    </button>
+                </div>
             </div>
+
         </div>
     </div>
 </template>
 
 <script>
+import { MagnifyingGlassCircleIcon } from "@heroicons/vue/16/solid";
 import api from "../services/api.js";
-import { useRoomStore } from "../stores/roomStore";
 
 export default {
     name: "Home",
@@ -55,32 +62,26 @@ export default {
             checkout: "",
             guest: "",
             room: "",
-            loading: false,
         };
     },
     methods: {
         async handleSearch() {
             this.loading = true;
-            const roomStore = useRoomStore();
 
             try {
-                const res = await api.post("/room/check-availability", {
-                    checkin: this.checkin,
-                    checkout: this.checkout,
-                    guest: this.guest,
-                    room: this.room,
-                });
-
-                // simpan ke store
-                roomStore.setAvailableRooms(res.data);
-
                 // pindah ke halaman available-room
                 console.log("Response API:", res.data)
-                this.$router.push({ name: "available-room" });
+                this.$router.push({
+                    name: "available-room", query: {
+                        checkin: this.checkin,
+                        checkout: this.checkout,
+                        guest: this.guest,
+                        room: this.room,
+                    }
+                });
             } catch (err) {
                 console.error(err);
             } finally {
-                this.loading = false;
             }
         },
     },
