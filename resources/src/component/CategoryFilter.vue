1756c362e21f50b4c0ef1c60f50eed2079e08b86
shafaadha <adhashafa@gmail.com>
2025-12-02 19:26:49 +0700

    feat(component): add category filter

---
+++ resources/src/component/CategoryFilter.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    modelValue: {
+        type: Array,
+        required: true
+    },
+    categories: {
+        type: Array,
+        required: true
+    }
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const selected = computed({
+    get: () => props.modelValue,
+    set: (val) => emit("update:modelValue", val)
+});
+</script>
+
+<template>
+    <div>
+        <h3 class="font-medium mb-2">Categories</h3>
+
+        <div v-for="cat in categories" :key="cat" class="flex items-center gap-2 mb-1 text-gray-900">
+            <input
+                type="checkbox"
+                :value="cat"
+                v-model="selected"
+                class="w-4 h-4 accent-cyan-600 rounded-full"
+            />
+            <label>{{ cat }}</label>
+        </div>
+    </div>
+</template>
