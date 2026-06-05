2bb22b625fbab0e58cfc838937bc0b14bf4fe839
shafaadha <adhashafa@gmail.com>
2026-06-06 02:00:15 +0700

    core: add dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,12 +17,14 @@ import * as HeroIconsOutline from "@heroicons/vue/24/outline";
 
 import { useAuthStore } from "../src/stores/auth.js";
 import { setAuthToken } from "../src/services/api.js";
+import VueApexCharts from "vue3-apexcharts";
 
 const app = createApp(App);
 
 const pinia = createPinia();
 
 app.use(pinia);
+app.use(VueApexCharts);
 
 const auth = useAuthStore();
 
